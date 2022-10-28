edb471c7c61f4bb951291360d59f9f9a7a44ab46
Adi Saric (SYD) <Adi.Saric@foxtel.com.au>
2022-10-28 12:30:11 +1100

    t push

---
+++ js/scripts.js
@@ -0,0 +1,139 @@
+json_data = {"queryId":"885dea339feefc13","hitCount":100,"hits":[{"id":"S_401689","title":{"en":"The Purge"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/655ke.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_401689:0","assetId":"S_401689","objectType":"Image","_version":10,"_lastModified":1664296703211}]},{"id":"S_389684","title":{"en":"Stupid Robot Fighting League"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/ahm4n.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_389684:0","assetId":"S_389684","objectType":"Image","_version":10,"_lastModified":1664310571563},{"contentType":"image/jpeg","url":"store2/mount1/16/3/ahm4n.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_389684:1","assetId":"S_389684","objectType":"Image","_version":10,"_lastModified":1664310571563}]},{"id":"S_197793","title":{"en":"Painkiller Jane"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/620el.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_197793:0","assetId":"S_197793","objectType":"Image","_version":4,"_lastModified":1659816937413}]},{"id":"S_354544","title":{"en":"Wynonna Earp"},"images":[{"contentType":"image/unknown","url":"store1/mount1/FBO/2018/I@081371_L.jpg","preferred":false,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_354544:0","assetId":"S_354544","objectType":"Image","_version":10,"_lastModified":1659816937431}]},{"id":"S_402548","title":{"en":"Happy!"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/7hzlx.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_402548:0","assetId":"S_402548","objectType":"Image","_version":6,"_lastModified":1659816937512}]},{"id":"S_392909","title":{"en":"Kepler"},"images":[{"contentType":"image/unknown","url":"store1/mount1/WPV/2020/I@107170_L.jpg","preferred":false,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_392909:0","assetId":"S_392909","objectType":"Image","_version":17,"_lastModified":1661668111027}]},{"id":"S_328155","title":{"en":"Dark Matter"},"guid":"1c59f0c6-a31c-491b-ad03-6c2e4c8238fb","images":[{"contentType":"image/jpeg","url":"store2/mount1/26/p11682468_b_h8_ac.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_328155:0","assetId":"S_328155","objectType":"Image","_version":34,"_lastModified":1666856556306},{"contentType":"image/jpeg","url":"store2/mount1/26/p11682468_b_h8_ac.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_328155:1","assetId":"S_328155","objectType":"Image","_version":34,"_lastModified":1666856556306}]},{"id":"S_179338","title":{"en":"Avatar: The Last Airbender"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/ay3ee.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_179338:0","assetId":"S_179338","objectType":"Image","_version":38,"_lastModified":1664310259550},{"contentType":"image/jpeg","url":"store2/mount1/16/3/ay3ee.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_179338:1","assetId":"S_179338","objectType":"Image","_version":38,"_lastModified":1664310259550}]},{"id":"S_201575","title":{"en":"Ben 10"},"images":[{"contentType":"image/png","url":"store2/mount1/16/3/9umfm.png","preferred":false,"width":10393,"height":5847,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_201575:0","assetId":"S_201575","objectType":"Image","_version":29,"_lastModified":1664691797524},{"contentType":"image/png","url":"store2/mount1/16/3/9umfm.png","preferred":false,"width":10393,"height":5847,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_201575:1","assetId":"S_201575","objectType":"Image","_version":29,"_lastModified":1664691797524}]},{"id":"S_278793","title":{"en":"Ninjago: Masters of Spinjitzu"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/2/882qo.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_278793:0","assetId":"S_278793","objectType":"Image","_version":82,"_lastModified":1664850680578},{"contentType":"image/jpeg","url":"store2/mount1/16/2/882qo.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_278793:1","assetId":"S_278793","objectType":"Image","_version":82,"_lastModified":1664850680578}]},{"id":"S_100762","title":{"en":"The Powerpuff Girls"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/9pe7u.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_100762:0","assetId":"S_100762","objectType":"Image","_version":43,"_lastModified":1665463858813},{"contentType":"image/jpeg","url":"store2/mount1/16/3/9pe7u.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_100762:1","assetId":"S_100762","objectType":"Image","_version":43,"_lastModified":1665463858813}]},{"id":"S_155916","title":{"en":"What's New, Scooby-Doo?"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/2/bbymd.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_155916:0","assetId":"S_155916","objectType":"Image","_version":47,"_lastModified":1666856548232},{"contentType":"image/jpeg","url":"store2/mount1/16/2/bbymd.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_155916:1","assetId":"S_155916","objectType":"Image","_version":47,"_lastModified":1666856548232}]},{"id":"S_249303","title":{"en":"Archer"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/4/91mba.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_249303:0","assetId":"S_249303","objectType":"Image","_version":213,"_lastModified":1666770363797},{"contentType":"image/jpeg","url":"store2/mount1/16/4/91mba.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_249303:1","assetId":"S_249303","objectType":"Image","_version":213,"_lastModified":1666770363797}]},{"id":"S_303123","title":{"en":"Spartacus: Blood and Sand"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/arw1j.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_303123:0","assetId":"S_303123","objectType":"Image","_version":29,"_lastModified":1664310278588},{"contentType":"image/jpeg","url":"store2/mount1/16/3/arw1j.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_303123:1","assetId":"S_303123","objectType":"Image","_version":29,"_lastModified":1664310278588}]},{"id":"S_225292","title":{"en":"Ben and James: On Thin Ice"},"images":[{"contentType":"image/png","url":"store2/mount1/16/4/b6bi2.png","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_225292:0","assetId":"S_225292","objectType":"Image","_version":41,"_lastModified":1665735415093},{"contentType":"image/png","url":"store2/mount1/16/4/b6bi2.png","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_225292:1","assetId":"S_225292","objectType":"Image","_version":41,"_lastModified":1665735415093}]},{"id":"S_114464","title":{"en":"From the Earth to the Moon"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/2/8n9tl.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_114464:0","assetId":"S_114464","objectType":"Image","_version":25,"_lastModified":1664310253200},{"contentType":"image/jpeg","url":"store2/mount1/16/2/8n9tl.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_114464:1","assetId":"S_114464","objectType":"Image","_version":25,"_lastModified":1664310253200}]},{"id":"S_285621","title":{"en":"The Legend of Korra"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/bb84g.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_285621:0","assetId":"S_285621","objectType":"Image","_version":124,"_lastModified":1664310272725},{"contentType":"image/jpeg","url":"store2/mount1/16/3/bb84g.jpg","preferred":false,"width":3840,"height":2160,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_285621:1","assetId":"S_285621","objectType":"Image","_version":124,"_lastModified":1664310272725}]},{"id":"S_264841","title":{"en":"Strike Back"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/ayeb8.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_264841:0","assetId":"S_264841","objectType":"Image","_version":155,"_lastModified":1664310266560},{"contentType":"image/jpeg","url":"store2/mount1/16/3/ayeb8.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_264841:1","assetId":"S_264841","objectType":"Image","_version":155,"_lastModified":1664310266560}]},{"id":"S_101534","title":{"en":"Band of Brothers"},"guid":"5cef959e-7beb-4666-bdb8-6754d31c7317","images":[{"contentType":"image/jpeg","url":"store2/mount1/16/2/a0sua.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_101534:0","assetId":"S_101534","objectType":"Image","_version":198,"_lastModified":1666770360435},{"contentType":"image/jpeg","url":"store2/mount1/16/2/a0sua.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_101534:1","assetId":"S_101534","objectType":"Image","_version":198,"_lastModified":1666770360435}]},{"id":"S_227114","title":{"en":"Wipeout"},"images":[{"contentType":"image/jpeg","url":"store2/mount1/16/3/7oa8s.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"title"},"id":"img:S_227114:0","assetId":"S_227114","objectType":"Image","_version":631,"_lastModified":1666883753545},{"contentType":"image/jpeg","url":"store2/mount1/16/3/7oa8s.jpg","preferred":false,"width":1920,"height":1080,"orientation":"LANDSCAPE","caption":{"en":"default"},"id":"img:S_227114:1","assetId":"S_227114","objectType":"Image","_version":631,"_lastModified":1666883753545}]}]};
+
+//var count = json_data.hitCount;
+//fillCards(json_data);
+/*
+const data = [
+  {
+    image: "./designs/todo.png",
+    title: "TODO App",
+    url: "https://hariramjp777.github.io/frontend-todo-app/",
+  },
+  {
+    image: "./designs/grid-component.png",
+    title: "Grid Component",
+    url: "https://hariramjp777.github.io/frontend-single-price-grid-component/",
+  },
+  {
+    image: "./designs/social-proof.png",
+    title: "Social Proof Section",
+    url: "https://hariramjp777.github.io/frontend-social-proof-section/",
+  },
+  {
+    image: "./designs/signup-form.png",
+    title: "SignUp Form",
+    url: "https://hariramjp777.github.io/frontend-intro-component-signup-form/",
+  },
+  {
+    image: "./designs/testimonial-grid.png",
+    title: "Testimonials Grid Layout Page",
+    url: "https://hariramjp777.github.io/frontend-testimonials-grid-section/",
+  },
+  {
+    image: "./designs/dashboard.png",
+    title: "Social Media Dashboard",
+    url:
+      "https://hariramjp777.github.io/frontend-social-media-dashboard-theme-switcher/",
+  },
+  {
+    image: "./designs/huddle.png",
+    title: "Landing Page",
+    url:
+      "https://hariramjp777.github.io/frontend-huddle-landing-page-intro-section/",
+  },
+  {
+    image: "./designs/fylo.png",
+    title: "Landing Page",
+    url: "https://hariramjp777.github.io/frontend-fylo-data-storage-component/",
+  },
+  {
+    image: "./designs/404.png",
+    title: "404 Page",
+    url: "https://hariramjp777.github.io/frontend-404-page-challenge/",
+  },
+  {
+    image: "./designs/team.png",
+    title: "Team Page",
+    url: "https://hariramjp777.github.io/frontend-team-page/",
+  },
+  {
+    image: "./designs/recipe.png",
+    title: "Recipe Page",
+    url: "https://hariramjp777.github.io/frontend-recipe-page/",
+  },
+  {
+    image: "./designs/card.png",
+    title: "Cards",
+    url: "https://hariramjp777.github.io/frontend-four-card-feature-section/",
+  },
+];
+*/
+document.addEventListener("DOMContentLoaded", function () {
+  fillCards(json_data);
+  const next = document.getElementById("next");
+  const prev = document.getElementById("prev");
+  next.addEventListener("click", function () {
+    const currCard = document.querySelector(".card.view");
+    const nextCard = currCard.nextElementSibling
+      ? currCard.nextElementSibling
+      : document.querySelector(".card-container").firstElementChild;
+    currCard.classList.remove("view");
+    nextCard.classList.add("view");
+  });
+
+  prev.addEventListener("click", function () {
+    const currCard = document.querySelector(".card.view");
+    const prevCard = currCard.previousElementSibling
+      ? currCard.previousElementSibling
+      : document.querySelector(".card-container").lastElementChild;
+    currCard.classList.remove("view");
+    prevCard.classList.add("view");
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prev.click();
+    else if (e.key === "ArrowRight") next.click();
+    else return false;
+  });
+
+  document.querySelector(".btn").addEventListener("click", function () {
+    const img = this.children[0];
+    document.querySelector(".about").classList.toggle("view");
+    setTimeout(function () {
+      img.setAttribute(
+        "src",
+        img.getAttribute("src") === "./images/icon-cross.svg"
+          ? "./images/icon-hamburger.svg"
+          : "./images/icon-cross.svg"
+      );
+    }, 800);
+  });
+});
+
+function fillCards(json_data) {
+
+  var count = json_data.hitCount;
+  response = json_data.hits;
+
+  const container = document.querySelector(".card-container");
+  response.forEach((data) => {
+    const card = document.createElement("div"),
+      cardImage = document.createElement("div"),
+      img = document.createElement("img"),
+      url = document.createElement("a");
+    img.setAttribute("src", "https://images2.resources.foxtel.com.au/" + data.images[0].url);
+    img.setAttribute("alt", data.title.en);
+    url.textContent = data.title.en;
+    //url.setAttribute("href", data.url);
+    url.setAttribute("target", "_blank");
+    card.classList.add("card");
+    cardImage.classList.add("card-image");
+    if (data.title.en === "The Purge") {
+      card.classList.add("view");
+    }
+    cardImage.appendChild(img);
+    card.appendChild(cardImage);
+    card.appendChild(url);
+    container.appendChild(card);
+  });
+}
